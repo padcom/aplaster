507255a68201a69f1520d763f7e17c67dad2d92b
padcom <padcom@4fbfdf0d-12f9-4140-a263-cb25e11bafc7>
2007-03-20 16:06:52 +0000

    [artf1028] Added library with tests (the new approach)

---
+++ unit-tests/tests.dpr
@@ -0,0 +1,20 @@
+library Tests;
+
+uses
+  ShareMem, Classes, SysUtils, TestFramework, 
+  ViewBuildersTest in 'test-cases\ViewBuildersTest.pas',
+  ConfigTest in 'test-cases\ConfigTest.pas',
+  ConfigFactoryTest in 'test-cases\ConfigFactoryTest.pas',
+  ConfigManipulatorTest in 'test-cases\ConfigManipulatorTest.pas',
+  ViewUtilsTest in 'test-cases\ViewUtilsTest.pas',
+  Script in 'test-cases\Script.pas',
+  ScriptDebug in 'test-cases\ScriptDebug.pas',
+  ScriptObjectsTest in 'test-cases\ScriptObjectsTest.pas',
+  ScriptObjectsTestEnvironment in 'test-cases\ScriptObjectsTestEnvironment.pas',
+  EditorsTest in 'test-cases\EditorsTest.pas';
+  
+exports
+  RegisteredTests name 'Test';
+  
+end.
+
